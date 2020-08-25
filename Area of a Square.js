45c9d9bc1c7233aca37987ac9f6d806e3d1484eb
JuliaVal821 <julia.kolgatina@gmail.com>
2020-08-25 15:24:00 -0400

    Found the area of a square

---
+++ Area of a Square.js
@@ -0,0 +1,8 @@
+https://www.codewars.com/kata/5748838ce2fab90b86001b1a/train/javascript
+
+    function squareArea(A){
+        let square;
+        let r = (A * 2) / Math.PI;
+        { square = Math.pow(r, 2); }
+        return +square.toFixed(2);
+    }
